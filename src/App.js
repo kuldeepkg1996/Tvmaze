5da965057fac68a60559d029f5a0c01df6717ecc
kuldeepkg1996 <kuldeepkg1996@gmail.com>
2022-09-24 02:50:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FrontPage from './Components/FrontPage';
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
